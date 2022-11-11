2105d375798519fd6911e7b12a0f0c12892e042e
TEZv <zazera777@gmail.com>
2022-11-11 21:45:48 +0200

    add index-1st-edition.js

---
+++ js/index-1st-edition.js
@@ -0,0 +1,12 @@
+// [x] Initialize scores
+
+            let compScore = 0;
+            let playerScore = 0;
+
+            // [] Initialize Selections
+
+            let playerSelection;
+            let computerSelection;
+
+            // [x] playRound() function - plays a single round of Rock Paper Scissors.  Function should take 2 params - playerSelection & computerSelection
+            // [x] And then return a string that declares the winner of the round liks so: "You Lose!  Paper beats Rock"

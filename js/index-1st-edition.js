7fe70578c694ca2997267b4eeed28c0427f281eb
TEZv <zazera777@gmail.com>
2022-11-12 08:46:15 +0200

    add index-1st-edition.js

--- js/index-1st-edition.js
+++ js/index-1st-edition.js
@@ -9,4 +9,87 @@
             let computerSelection;
 
             // [x] playRound() function - plays a single round of Rock Paper Scissors.  Function should take 2 params - playerSelection & computerSelection
-            // [x] And then return a string that declares the winner of the round liks so: "You Lose!  Paper beats Rock"
+            // [x] And then return a string that declares the winner of the round liks so: "You Lose!  Paper beats Rock" 
+            function playRound(playerSelection, computerSelection){
+
+                
+                // [x] computer makes selection
+
+                computerSelection = computerPlay();
+
+            // [x] Compare computerSelection to playerSelection.  Determine who wins
+
+                switch(computerSelection){
+                    case "Rock":
+                        switch(playerSelection){
+                            case "Rock":
+                            
+                                
+                                return "You Tied!  " + playerSelection + " is equal to " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+                            case "Paper":
+
+                                playerScore++;
+                                return "You Won!  " + playerSelection + " beats " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+                            case "Scissors":
+
+                                compScore++;
+
+
+                                return "You Lose!  " + computerSelection + " beats " + playerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+                        }
+
+                    case "Paper":
+                        switch(playerSelection){
+                            case "Rock":
+                            
+                                compScore++;
+
+
+                                return "You Lose!  " + computerSelection + " beats " + playerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+                            case "Paper":
+
+                                return "You Tied!  " + playerSelection + " is equal to " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+
+                            case "Scissors":
+
+                                playerScore++;
+
+                                return "You Won!  " + playerSelection + " beats " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+                        }
+
+                    case "Scissors":
+                    switch(playerSelection){
+                            case "Rock":
+                            
+                                playerScore++;
+
+                                return "You Won!  " + playerSelection + " beats " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;
+                                break;
+
+                            case "Paper":
+
+                                compScore++;
+
+
+                                return "You Lose!  " + computerSelection + " beats " + playerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+                            case "Scissors":
+
+                                return "You Tied!  " + playerSelection + " is equal to " + computerSelection + "!" + " Score: " + playerScore + " to " + compScore;;
+                                break;
+
+                        }
+
+                }
+

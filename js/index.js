6169231fcace48f3e8eebb7f9e0ad47fe01f49e5
TEZv <zazera777@gmail.com>
2022-11-12 09:58:28 +0200

    add js

--- js/index.js
+++ js/index.js
@@ -1,66 +1,61 @@
 // Rotate images 90 degrees
 
-                let images = document.querySelectorAll('img');
+let images = document.querySelectorAll("img");
 
-                images.forEach(img => img.style.transform = "rotate(90deg)");
-                
-                // Initialize playerSelection & computerSelection
+images.forEach((img) => (img.style.transform = "rotate(90deg)"));
 
+// Initialize playerSelection & computerSelection
 
-                let playerSelection, computerSelection;
+let playerSelection, computerSelection;
 
-                // Initialize scores
+// Initialize scores
 
-                let compScore = 0;
-                let playerScore = 0;
+let compScore = 0;
+let playerScore = 0;
 
-                // Initialize output div
+// Initialize output div
 
-                let response = document.querySelector('#results');
+let response = document.querySelector("#results");
 
-            // Initialize # of Wins
+// Initialize # of Wins
 
-            let roundSelector;
+let roundSelector;
 
-            let numberOfRounds = 5;
+let numberOfRounds = 5;
 
-            roundSelector = document.querySelector('#roundInput'); 
+roundSelector = document.querySelector("#roundInput");
 
-            console.log("First Round Selector: " + roundSelector.value);
+console.log("First Round Selector: " + roundSelector.value);
 
-            // Initialize finalRoundsMessage
+// Initialize finalRoundsMessage
 
-            let finalRoundsMessage;
-            
-            // Change # of Wins on select
-            
-            roundSelector.addEventListener("change", (e) => {
+let finalRoundsMessage;
 
-                    numberOfRounds = roundSelector.options[roundSelector.selectedIndex].getAttribute('value');
+// Change # of Wins on select
 
-                    console.log("Second Round Selector: " + numberOfRounds);
+roundSelector.addEventListener("change", (e) => {
+  numberOfRounds =
+    roundSelector.options[roundSelector.selectedIndex].getAttribute("value");
 
-                    return numberOfRounds;
+  console.log("Second Round Selector: " + numberOfRounds);
 
-            });
-                // Make buttons clickable - on click, play round
-           
-            let buttons = document.querySelectorAll('.button');
+  return numberOfRounds;
+});
+// Make buttons clickable - on click, play round
 
-            buttons.forEach((button) => {
-                button.addEventListener("click", (e) => {
+let buttons = document.querySelectorAll(".button");
 
-                    playRound(button.getAttribute('value'), computerPlay(), numberOfRounds);
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playRound(button.getAttribute("value"), computerPlay(), numberOfRounds);
+  });
+});
 
-                })
-                });
-           
-            //testReturnValues(compScore, playerScore, playerSelection, computerSelection);
+//testReturnValues(compScore, playerScore, playerSelection, computerSelection);
 
-            //getPlayerSelection();
+//getPlayerSelection();
 
-
-            /*function testReturnValues(compScore, playerScore, playerSelection, computerSelection){
+/*function testReturnValues(compScore, playerScore, playerSelection, computerSelection){
                 console.log("playerSelection: " + playerSelection);
                 console.log("computerSelection: " + computerSelection);
                 console.log("compScore: " + compScore);
@@ -68,26 +63,25 @@
             };
             */
 
-       
-            // [x] Computer choice - computerPlay() function - randomly return either 'Rock', 'Paper' or 'Scissors'.
+// [x] Computer choice - computerPlay() function - randomly return either 'Rock', 'Paper' or 'Scissors'.
 
-            function computerPlay(){
-                numberSelection = Math.floor(Math.random() * 3);
+function computerPlay() {
+  numberSelection = Math.floor(Math.random() * 3);
 
-                switch(numberSelection){
-                    case 0:
-                        return "Rock";
-                        break;
-                    case 1:
-                        return "Paper";
-                        break;
-                    case 2:
-                        return "Scissors";
-                        break;
-                }
-};
-             // Make input case - insensitive
-            /*
+  switch (numberSelection) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+  }
+}
+// Make input case - insensitive
+/*
             function caseInsensitive(playerInput){
                 playerFirstLetter = playerInput.charAt(0).toUpperCase();
                 playerRemainingWord = playerInput.substr(1).toLowerCase();
@@ -95,20 +89,237 @@
                 return playerInput;
             }
             */
-            
-            // Play Round
 
-            function playRound(playerSelection, computerSelection, numberOfRounds){
+// Play Round
 
-                // Get Computer Choice & Convert computerPlay() -> to variable "computerSelection"
+function playRound(playerSelection, computerSelection, numberOfRounds) {
+  // Get Computer Choice & Convert computerPlay() -> to variable "computerSelection"
 
-                // Get User Choice & Convert getPlayerInput() -> to variable "playerSelection"
+  // Get User Choice & Convert getPlayerInput() -> to variable "playerSelection"
 
-                // playerSelection = document.getElementById.value;
+  // playerSelection = document.getElementById.value;
 
-                // playerSelection = getPlayerInput(playerClick);
+  // playerSelection = getPlayerInput(playerClick);
 
-                // Computer & Player choices made
+  // Computer & Player choices made
 
-                console.log("Computer Choice: " + computerSelection);
-                console.log("Player Choice: " + playerSelection);
+  console.log("Computer Choice: " + computerSelection);
+  console.log("Player Choice: " + playerSelection);
+  // Comparison
+
+  response.innerHTML = roundWinner(playerSelection, computerSelection);
+
+  function roundWinner(playerSelection, computerSelection) {
+    switch (computerSelection) {
+      case "Rock":
+        switch (playerSelection) {
+          case "Rock":
+            return (
+              "You Tied!  " +
+              playerSelection +
+              " is equal to " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Paper":
+            playerScore++;
+
+            return (
+              "You Won!  " +
+              playerSelection +
+              " beats " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Scissors":
+            compScore++;
+
+            return (
+              "You Lose!  " +
+              computerSelection +
+              " beats " +
+              playerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+        }
+
+      case "Paper":
+        switch (playerSelection) {
+          case "Rock":
+            compScore++;
+
+            return (
+              "You Lose!  " +
+              computerSelection +
+              " beats " +
+              playerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Paper":
+            return (
+              "You Tied!  " +
+              playerSelection +
+              " is equal to " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Scissors":
+            playerScore++;
+
+            return (
+              "You Won!  " +
+              playerSelection +
+              " beats " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+        }
+
+      case "Scissors":
+        switch (playerSelection) {
+          case "Rock":
+            playerScore++;
+
+            return (
+              "You Won!  " +
+              playerSelection +
+              " beats " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Paper":
+            compScore++;
+
+            return (
+              "You Lose!  " +
+              computerSelection +
+              " beats " +
+              playerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+
+          case "Scissors":
+            return (
+              "You Tied!  " +
+              playerSelection +
+              " is equal to " +
+              computerSelection +
+              "!" +
+              "<br>" +
+              "<br>" +
+              "Computer: " +
+              compScore +
+              " vs. Player: " +
+              playerScore
+            );
+            break;
+        }
+    }
+  }
+
+  // Run game - Play 5 Rounds
+  /*
+                function game(){
+                    console.log(playRound(playerSelection, computerSelection));
+                    console.log(playRound(playerSelection, computerSelection));
+                    console.log(playRound(playerSelection, computerSelection));
+                    console.log(playRound(playerSelection, computerSelection));
+                    console.log(playRound(playerSelection, computerSelection));
+                
+                }
+                console.log(game());
+                */
+
+  // Track the final winner of the game
+
+  if (compScore == numberOfRounds) {
+    finalRoundsMessage =
+      "Sorry, the computer won the game.  The final score was:" +
+      "<br>" +
+      "<br>" +
+      "Computer: " +
+      compScore +
+      " Player: " +
+      playerScore;
+    console.log("computer won");
+
+    var finalOutput = document.querySelector("#finalResults");
+
+    finalOutput.innerHTML = finalRoundsMessage;
+  } else if (playerScore == numberOfRounds) {
+    finalRoundsMessage =
+      "Congratulations!  You won the game!  The final score was:" +
+      "<br>" +
+      "<br>" +
+      "Computer: " +
+      compScore +
+      " Player: " +
+      playerScore;
+    console.log("you won");
+
+    let finalOutput = document.querySelector("#finalResults");
+
+    finalOutput.innerHTML = finalRoundsMessage;
+  }
+}
